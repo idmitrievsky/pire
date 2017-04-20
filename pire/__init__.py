6ebe37b44128152a5ef7e5121a2fba8b5e31874e
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2017-04-20 12:19:21 +0300

    Makie importing directly from pire possible

---
+++ pire/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from pire.core import excepting, skipping, finally_call, supervise, supervised
+
+__all__ = ['excepting', 'skipping', 'finally_call', 'supervise', 'supervised']
